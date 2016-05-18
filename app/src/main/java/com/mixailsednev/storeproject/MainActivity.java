915778d3016a5c2e78a04f255b5d2f75b2ef5b7a
Michail Sednev <sednevmixail@gmail.com>
2016-05-18 17:35:19 +0400

    icons + refactor toolbar

--- app/src/main/java/com/mixailsednev/storeproject/MainActivity.java
+++ app/src/main/java/com/mixailsednev/storeproject/MainActivity.java
@@ -7,17 +7,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements ProductListFragment.ProductSelectedListener{
-
+public class MainActivity extends AppCompatActivity
+        implements ProductListFragment.ProductSelectedListener, Toolbar.OnMenuItemClickListener
+{
     private boolean mTwoPane;
 
     protected DrawerLayout mDrawerLayout;
@@ -48,39 +46,27 @@ public class MainActivity extends AppCompatActivity implements ProductListFragme
             }
         });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-
-        final ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayShowTitleEnabled(false);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setTitle(getString(R.string.products));
-        }
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mainToolbar.inflateMenu(R.menu.main_menu);
+        mainToolbar.setTitle(getString(R.string.products));
+        mainToolbar.setNavigationIcon(R.drawable.ic_menu);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mainToolbar,
                 R.string.open_navigation_drawer, R.string.close_navigation_drawer) {
         };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
+
+        Toolbar detailsToolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        detailsToolbar.inflateMenu(R.menu.details_menu);
+        detailsToolbar.setOnMenuItemClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public boolean onMenuItemClick(MenuItem item) {
+        return false;
     }
 
     @Override
